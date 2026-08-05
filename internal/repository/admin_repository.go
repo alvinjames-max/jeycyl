9f0b0581655c1ad907f6652e15c3b853ee2f9f74
alvagot <agot.alvinjames@gmail.com>
2026-08-05 10:29:08 +0300

    add create method to adminrepository

--- internal/repository/admin_repository.go
+++ internal/repository/admin_repository.go
@@ -14,3 +14,20 @@ type AdminRepository struct {
 func NewAdminRepository(db *sql.DB) *AdminRepository {
 	return &AdminRepository{db: db}
 }
+
+func (r *AdminRepository) Create(a *models.Admin) (int64, error) {
+	res, err := r.db.Exec(`
+		INSERT INTO admins (username, password_hash)
+		VALUES (?, ?)
+	`, a.Username, a.PasswordHash)
+	if err != nil {
+		return 0, fmt.Errorf("creating admin: %w", err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("getting new admin id: %w", err)
+	}
+
+	return id, nil
+}
